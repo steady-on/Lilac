f941a42b6b028391c772492e6d8daf03b17fb122
steady-on <roen.steady.on@gmail.com>
2024-01-03 23:44:31 +0900

    init: create Project & target settings

---
+++ Lilac/Lilac/LilacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilacApp.swift
+//  Lilac
+//
+//  Created by Roen White on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LilacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
