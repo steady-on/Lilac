221258a08ec133a3f64fb51cb0147dedb8e54527
steady-on <roen.steady.on@gmail.com>
2024-01-04 18:54:43 +0900

    init: add project file

---
+++ Lilac/Lilac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lilac
+//
+//  Created by Roen White on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
