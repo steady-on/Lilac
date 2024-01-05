f8e7c598a5b8d8345846e1371f8fdd72490608d7
steady-on <roen.steady.on@gmail.com>
2024-01-05 18:34:16 +0900

    feat(NetworkMonitor): 앱의 실행과 종료시에 네트워크 상태 모니터링을 시작하고 종료하도록 함

--- Lilac/Lilac/SceneDelegate.swift
+++ Lilac/Lilac/SceneDelegate.swift
@@ -16,6 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        NetworkMonitor.shared.startMonitoring()
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -24,6 +26,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
+        NetworkMonitor.shared.stopMonitoring()
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
