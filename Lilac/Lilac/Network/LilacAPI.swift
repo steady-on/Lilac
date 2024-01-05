9f6fa1f8fc2143655a50453f5c2d3bddb3256838
steady-on <roen.steady.on@gmail.com>
2024-01-06 04:06:55 +0900

    update(LilacAPI): User 열거형 추가
    - 회원에 관련된 요청을 처리하는 User열거형 추가
    - 회원가입 시 유저의 정보를 담을 NewUser 구조체 구현

--- Lilac/Lilac/Network/LilacAPI.swift
+++ Lilac/Lilac/Network/LilacAPI.swift
@@ -11,4 +11,26 @@ enum LilacAPI {
     enum Auth: String {
         case refresh
     }
+    
+    enum User {
+        case signUp(userInfo: Requester.NewUser)
+        case validateEmail(email: String)
+        case signIn(vendor: Vendor)
+        case signOut
+        case saveDeviceToken
+        case myProfile(type: WorkType)
+        case askForOtherUserProfile(id: Int)
+        
+        enum Vendor {
+            case email(email: String, password: String)
+            case kakao
+            case apple
+        }
+        
+        enum WorkType {
+            case askForMyInfo
+            case editInfo(nickname: String?, phone: String?)
+            case editImage(image: Data)
+        }
+    }
 }
---
+++ Lilac/Lilac/Network/Model/Requester.swift
@@ -0,0 +1,18 @@
+//
+//  Requester.swift
+//  Lilac
+//
+//  Created by Roen White on 1/6/24.
+//
+
+import Foundation
+
+struct Requester {
+    struct NewUser: Encodable {
+        let email: String
+        let password: String
+        let nickname: String
+        let phone: String
+        let deviceToken: String
+    }
+}
