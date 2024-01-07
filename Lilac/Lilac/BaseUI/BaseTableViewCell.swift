29fa718186ae188c1fd536a73180870617d6c551
steady-on <roen.steady.on@gmail.com>
2024-01-07 22:03:47 +0900

    add: BaseTableViewCell

---
+++ Lilac/Lilac/BaseUI/BaseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseTableViewCell.swift
+//  Lilac
+//
+//  Created by Roen White on 1/7/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureHiararchy()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHiararchy() {}
+    func setConstraints() {}
+}
