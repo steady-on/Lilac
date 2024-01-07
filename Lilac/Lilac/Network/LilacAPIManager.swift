113535ecf545325958a60401810d59f0bdd35c11
steady-on <roen.steady.on@gmail.com>
2024-01-07 21:43:51 +0900

    update(LilacAPIManager): add method; request(_:responder:) -> Single<Result<U, Error>>
    - 응답값이 있는 요청을 수행하기위한 메서드

--- Lilac/Lilac/Network/LilacAPIManager.swift
+++ Lilac/Lilac/Network/LilacAPIManager.swift
@@ -13,6 +13,29 @@ struct LilacAPIManager<T: TargetType> {
     
     private let provider = MoyaProvider<T>()
     
+    func request<U: Decodable>(_ api: T, responder: U.Type) -> Single<Result<U, Error>> {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        return Single<Result<U, Error>>.create { single in
+            self.provider.request(api) { result in
+                switch result {
+                case .success(let response):
+                    guard let decodedData = try? decoder.decode(U.self, from: response.data) else {
+                        let error = parseErrorData(response.data)
+                        single(.success(.failure(error)))
+                        return
+                    }
+                    
+                    single(.success(.success(decodedData)))
+                case .failure(let moyaError):
+                    single(.failure(moyaError))
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
     
     func request(_ api: T) -> Completable {
         return Completable.create { completable in
