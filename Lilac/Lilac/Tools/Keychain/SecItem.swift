43e5eff3e6a7221bdefb1f5730ba524f0bcdb399
steady-on <roen.steady.on@gmail.com>
2024-01-08 17:42:45 +0900

    add: SecItem
    - 키체인에 저장할 정보를 담는 구조체와 종류 정의

---
+++ Lilac/Lilac/Tools/Keychain/SecItem.swift
@@ -0,0 +1,19 @@
+//
+//  SecItem.swift
+//  Lilac
+//
+//  Created by Roen White on 1/8/24.
+//
+
+import Foundation
+
+struct SecItem {
+    let type: ItemType
+    let value: String
+}
+
+enum ItemType: String {
+    case accessToken
+    case refreshToken
+    case password
+}
