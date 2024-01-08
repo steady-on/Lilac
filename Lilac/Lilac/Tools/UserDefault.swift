94d58fd27956071a5d6dd28e896f0ab81781badc
steady-on <roen.steady.on@gmail.com>
2024-01-08 18:00:47 +0900

    add: UserDefault property wrapper

---
+++ Lilac/Lilac/Tools/UserDefault.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaults.swift
+//  Lilac
+//
+//  Created by Roen White on 1/8/24.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    private let key: String
+    private let defaultValue: T
+    
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
