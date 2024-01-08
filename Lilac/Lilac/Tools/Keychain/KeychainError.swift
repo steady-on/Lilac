4a4f9885116738686668fb6c78bdcf7e998b0a14
steady-on <roen.steady.on@gmail.com>
2024-01-08 17:43:05 +0900

    add: KeychainError

---
+++ Lilac/Lilac/Tools/Keychain/KeychainError.swift
@@ -0,0 +1,16 @@
+//
+//  KeychainError.swift
+//  Lilac
+//
+//  Created by Roen White on 1/8/24.
+//
+
+import Foundation
+
+enum KeychainError: Error {
+    case failToAddItemError(itemType: String)
+    case noItem(itemType: String)
+    case failToSearchItemError(itemType: String)
+    case unexpectedItemData(status: OSStatus)
+    case unhandledError(status: OSStatus)
+}
