6aa0e454466b8aa5d10de1e4b192bbde679a5e08
steady-on <roen.steady.on@gmail.com>
2024-01-14 18:16:01 +0900

    init: settings for kakaoSDK

--- Lilac/Lilac/AppDelegate.swift
+++ Lilac/Lilac/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxKakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        RxKakaoSDK.initSDK(appKey: "87147aeac243b910ef2d620dba5ff2bc")
+        
         return true
     }
 
