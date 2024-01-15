cb8665e77465ec1cde84caef65630c1901ff6d6f
steady-on <roen.steady.on@gmail.com>
2024-01-15 16:40:34 +0900

    add: LilacUserService
    - Lilac 서버의 Auth, User와 관련된 비즈니스 로직을 답고 있는 Service

---
+++ Lilac/Lilac/Service/LilacUserService.swift
@@ -0,0 +1,17 @@
+//
+//  LilacUserService.swift
+//  Lilac
+//
+//  Created by Roen White on 1/15/24.
+//
+
+import Foundation
+import RxSwift
+
+final class LilacUserService {
+    
+    func kakaoLogin(for accessToken: String) -> Single<Result<Responder.MyProfile, Error>> {
+        return LilacAPIManager<LilacAPI.User>().request(.signIn(vendor: .kakao(accessToken: accessToken)), responder: Responder.MyProfile.self)
+    }
+    
+}
