2affc1efd9076491617f4688bc04556d055c7642
steady-on <roen.steady.on@gmail.com>
2024-01-16 23:43:47 +0900

    add(String+): 비밀번호 유효성 여부를 담은 프로퍼티

--- Lilac/Lilac/Extension/String+.swift
+++ Lilac/Lilac/Extension/String+.swift
@@ -13,4 +13,10 @@ extension String {
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPredicate.evaluate(with: self)
     }
+    
+    var isValidPassword: Bool {
+        let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: self)
+    }
 }
