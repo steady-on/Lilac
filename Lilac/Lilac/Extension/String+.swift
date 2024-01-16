1c46d055bf317ac1c9814333a847251f5a039e35
steady-on <roen.steady.on@gmail.com>
2024-01-16 23:05:42 +0900

    add(String+): 이메일 주소로 유효한지에 대한 연산 프로퍼티 추가

---
+++ Lilac/Lilac/Extension/String+.swift
@@ -0,0 +1,16 @@
+//
+//  String+.swift
+//  Lilac
+//
+//  Created by Roen White on 1/16/24.
+//
+
+import Foundation
+
+extension String {
+    var isValidEmail: Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: self)
+    }
+}
