b0038ecae691802b833d149c3513cca9c356c8e3
steady-on <roen.steady.on@gmail.com>
2024-01-18 11:40:29 +0900

    update(Responder): EmailDuplicateCheckResult 삭제

--- Lilac/Lilac/Network/Model/Responder.swift
+++ Lilac/Lilac/Network/Model/Responder.swift
@@ -12,10 +12,6 @@ enum Responder {
         let accessToken: String
     }
     
-    struct EmailDuplicateCheckResult: Decodable {
-        let email: String
-    }
-    
     struct SignUp: Decodable {
         let token: Token
     }
