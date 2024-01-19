1c16eb1e2467cc4e11ac67b088df9e82bbf2d116
steady-on <roen.steady.on@gmail.com>
2024-01-19 22:48:45 +0900

    update(String): 핸드폰 번호 사이에 -을 넣어주는 메서드 추가

--- Lilac/Lilac/Extension/String+.swift
+++ Lilac/Lilac/Extension/String+.swift
@@ -19,4 +19,40 @@ extension String {
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
         return passwordPredicate.evaluate(with: self)
     }
+    
+    private var decimalFilteredString: String {
+        return String(unicodeScalars.filter(CharacterSet.decimalDigits.contains))
+    }
+}
+
+extension String {
+    func formattedPhoneNumber() -> String {
+        let digit: Character = "#"
+ 
+        let pattern: [Character] = self.count < 13 ? Array("###-###-####") : Array("###-####-####")
+        let input: [Character] = Array(self.decimalFilteredString)
+        var formatted: [Character] = []
+ 
+        var patternIndex = 0
+        var inputIndex = 0
+ 
+        while inputIndex < input.count {
+            let inputCharacter = input[inputIndex]
+ 
+            guard patternIndex < pattern.count else { break }
+ 
+            switch pattern[patternIndex] == digit {
+            case true:
+                formatted.append(inputCharacter)
+                inputIndex += 1
+            case false:
+                formatted.append(pattern[patternIndex])
+            }
+ 
+            patternIndex += 1
+        }
+ 
+        return String(formatted)
+    }
+
 }
