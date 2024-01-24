47c530c399547fe7c482341ee75415e315466af0
steady-on <roen.steady.on@gmail.com>
2024-01-24 14:29:16 +0900

    refactor(WorkSpace): 구조 변경 및 case 세분화

--- Lilac/Lilac/Network/LilacAPI.swift
+++ Lilac/Lilac/Network/LilacAPI.swift
@@ -35,22 +35,20 @@ enum LilacAPI {
     }
     
     enum WorkSpace {
-        case workSpace(type: WorkSpace)
+        case create(name: String, description: String?, image: String)
+        case loadAll
+        case load(id: Int)
+        case update(id: Int, name: String?, description: String?, image: String?)
+        case delete(id: Int)
+        case search(id: Int, keyword: String)
+        case leave(id: Int)
+        case admin(id: Int, userId: Int)
         case member(id: Int, type: Member)
-        
-        enum WorkSpace {
-            case create(name: String, description: String?, image: String)
-            case load(id: Int?)
-            case update(id: Int, name: String?, description: String?, image: String?)
-            case delete(id: Int)
-        }
         
         enum Member {
             case invite(email: String)
-            case load(userId: Int?)
-            case search(keyword: String)
-            case leave
-            case admin(userId: Int)
+            case loadAll
+            case load(userId: Int)
         }
     }
 }
