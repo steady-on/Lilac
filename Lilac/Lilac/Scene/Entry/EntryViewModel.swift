0e894ccdce11ec6efa6c93fbf30d1e44675caefe
steady-on <roen.steady.on@gmail.com>
2024-01-26 19:05:13 +0900

    add: EntryViewModel 구현
    - 앱 실행 시 UserDefault의 isFirst 값이 true인 경우 Keychain에 있는 토큰 값들을 삭제하고 isFirst값을 false로 변경
    - refreshToken이 없으면 OnboardingView로 이동하도록 함
    - refreshToken이 있는 경우 accessToken을 refresh하고 HomeView로 진입하도록 함

--- Lilac/Lilac/Scene/Entry/EntryViewModel.swift
+++ Lilac/Lilac/Scene/Entry/EntryViewModel.swift
@@ -7,19 +7,65 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 final class EntryViewModel {
-    var disposeBag = DisposeBag()
     
-    private lazy var networkMonitor = NetworkMonitor.shared
+    deinit {
+        print("deinit EntryViewModel")
+    }
+    
+    @KeychainStorage(itemType: .refreshToken) private var refreshToken
+    @KeychainStorage(itemType: .accessToken) private var accessToken
+    
+    @UserDefault(key: .isFirst, defaultValue: true) private var isFirst
+    
+    private lazy var lilacAuthSevice = LilacAuthService.shared
+    
+    var disposeBag = DisposeBag()
 }
 
 extension EntryViewModel: ViewModel {
     struct Input {}
     
-    struct Output {}
+    struct Output {
+        let goToOnboardingView: BehaviorRelay<Bool>
+    }
     
     func transform(input: Input) -> Output {
+        let goToOnboardingView = BehaviorRelay<Bool>(value: true)
         
+        guard isFirst == false else {
+            refreshToken = nil
+            accessToken = nil
+            isFirst = false
+            return Output(goToOnboardingView: goToOnboardingView)
+        }
+        
+        guard refreshToken != nil else {
+            return Output(goToOnboardingView: goToOnboardingView)
+        }
+        
+        let tokenRefreshResponse = lilacAuthSevice.refreshAccessToken()
+        
+        tokenRefreshResponse
+            .subscribe(with: self) { owner, result in
+                switch result {
+                case .success(let newToken):
+                    owner.accessToken = newToken.accessToken
+                    goToOnboardingView.accept(false)
+                case .failure(let error):
+                    if case LilacAPIError.validToken = error {
+                        goToOnboardingView.accept(false)
+                    } else {
+                        goToOnboardingView.accept(true)
+                    }
+                }
+            } onFailure: { owner, error in
+                goToOnboardingView.accept(true)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(goToOnboardingView: goToOnboardingView)
     }
 }
