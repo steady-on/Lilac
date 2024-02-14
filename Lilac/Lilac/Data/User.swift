e155361afbd1e6bbbece544f9fa9a02d4b1076f2
steady-on <roen.steady.on@gmail.com>
2024-02-14 18:32:41 +0900

    add: class User

---
+++ Lilac/Lilac/Data/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Lilac
+//
+//  Created by Roen White on 2/13/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class User {
+    static let `default` = User()
+    private init() {}
+    
+    private let profile = PublishRelay<Responder.User.MyProfile>()
+    
+    func update(for profile: Responder.User.MyProfile) {
+        self.profile.accept(profile)
+    }
+}
