a792a0967f0fd9187732c1ecb28fa81e8a49799d
steady-on <roen.steady.on@gmail.com>
2024-02-17 01:21:38 +0900

    add: WelcomeViewModel

---
+++ Lilac/Lilac/Scene/Onboarding/SignUp/WelcomeViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeViewModel.swift
+//  Lilac
+//
+//  Created by Roen White on 2/17/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class WelcomeViewModel {
+    var disposeBag = DisposeBag()
+    
+    private lazy var lilacWorkSpaceService = LilacWorkSpaceService()
+}
+
+extension WelcomeViewModel: ViewModel {
+    struct Input {
+        let closeButtonTapped: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let goToHome: PublishRelay<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let goToHome = PublishRelay<Void>()
+        
+        input.closeButtonTapped
+            .flatMap { [unowned self] _ in
+                lilacWorkSpaceService.loadAll()
+            }
+            .subscribe { result in
+                switch result {
+                case .success(let workspaces):
+                    User.shared.fetch(for: workspaces)
+                case .failure(_):
+                    break
+                }
+                
+                goToHome.accept(())
+                
+            } onError: { _ in
+                goToHome.accept(())
+            }
+            .disposed(by: disposeBag)
+
+        
+        return Output(goToHome: goToHome)
+    }
+}
