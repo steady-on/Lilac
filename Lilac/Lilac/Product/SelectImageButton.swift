dc98c4c22151cb56048f7591b72d9b9c03965578
steady-on <roen.steady.on@gmail.com>
2024-02-19 18:14:22 +0900

    add: SelectImageButton 구현
    - 이미지 선택을 위한 버튼을 product로 구현

---
+++ Lilac/Lilac/Product/SelectImageButton.swift
@@ -0,0 +1,69 @@
+//
+//  SelectImageButton.swift
+//  Lilac
+//
+//  Created by Roen White on 2/19/24.
+//
+
+import UIKit
+
+final class SelectImageButton: UIButton {
+    private let baseImage: UIImage?
+    private var firstLayout = true
+    
+    init(baseImage: UIImage?) {
+        self.baseImage = baseImage
+        
+        super.init(frame: .init(x: 0, y: 0, width: 70, height: 70))
+        
+        configureButton()
+    }
+    
+    private let badgeImage = UIImageView(image: .Button.camera)
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: bounds.width, height: bounds.height)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        overlayBadge()
+    }
+    
+    private func configureButton() {
+        addSubview(badgeImage)
+        bringSubviewToFront(badgeImage)
+        
+        let image = baseImage ?? .WorkSpace.default
+        
+        var config = UIButton.Configuration.filled()
+        config.background.cornerRadius = 8
+        configuration = config
+        
+        setImage(image, for: .normal)
+    }
+    
+    private func overlayBadge() {
+        badgeImage.frame = CGRect(x: bounds.width * 0.8, y: bounds.height * 0.8, width: bounds.width * 0.35, height: bounds.height * 0.35)
+        bringSubviewToFront(badgeImage)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SelectImageButton {
+    func changeImage(for image: UIImage?) {
+        let imageSize = CGSize(width: bounds.width, height: bounds.height)
+        let resizedImage = image?.resize(targetSize: imageSize)
+
+        imageView?.contentMode = .scaleToFill
+        imageView?.clipsToBounds = true
+        imageView?.layer.cornerRadius = 8
+        
+        setImage(resizedImage, for: .normal)
+    }
+}
