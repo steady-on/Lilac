96da1c4300f10cb60e301ea7f40c85b6e618cd92
steady-on <roen.steady.on@gmail.com>
2024-02-22 16:55:52 +0900

    add: ProfileButton

---
+++ Lilac/Lilac/Product/ProfileButton.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileButton.swift
+//  Lilac
+//
+//  Created by Roen White on 2/22/24.
+//
+
+import UIKit
+
+final class ProfileButton: UIButton {
+    init() {
+        super.init(frame: .init(x: 0, y: 0, width: 32, height: 32))
+        
+        configureButton()
+    }
+    
+    private func configureButton() {
+        var config = UIButton.Configuration.plain()
+        config.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+        configuration = config
+        
+        imageView?.backgroundColor = .accent
+        imageView?.contentMode = .scaleAspectFill
+        imageView?.clipsToBounds = true
+        imageView?.layer.cornerRadius = bounds.width / 2
+        imageView?.layer.borderWidth = 2
+        imageView?.layer.borderColor = UIColor.View.selected.cgColor
+    }
+    
+    func setProfile(for image: UIImage) {
+        configuration?.image = image.resize(targetSize: .init(width: 32, height: 32))
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
